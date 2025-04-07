ac95238aa8ecfd415bfca59c3ba1c0b7c71edd55
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2025-04-07 14:33:16 -0700

    save

--- app/src/main/java/com/klee/volumelockr/ui/MainActivity.kt
+++ app/src/main/java/com/klee/volumelockr/ui/MainActivity.kt
@@ -8,8 +8,15 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.color.DynamicColors
 import com.klee.volumelockr.R
@@ -19,7 +26,54 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container_view)) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            // Apply the insets as padding to the view. Here, set all the dimensions
+            // as appropriate to your layout. You can also update the view's margin if
+            // more appropriate.
+            view.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
+
+            // Return CONSUMED if you don't want the window insets to keep passing down
+            // to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, insets ->
+//            val bars = insets.getInsets(
+//                WindowInsetsCompat.Type.systemBars()
+//                        or WindowInsetsCompat.Type.displayCutout()
+//            )
+//            v.updatePadding(
+//                left = bars.left,
+//                top = bars.top,
+//                right = bars.right,
+//                bottom = bars.bottom,
+//            )
+//            WindowInsetsCompat.CONSUMED
+//        }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main)) { v, windowInsets ->
+//            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            // Apply the insets as a margin to the view. This solution sets
+//            // only the bottom, left, and right dimensions, but you can apply whichever
+//            // insets are appropriate to your layout. You can also update the view padding
+//            // if that's more appropriate.
+//            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+//                topMargin = insets.top
+//                leftMargin = insets.left
+//                bottomMargin = insets.bottom
+//                rightMargin = insets.right
+//            }
+//
+//            // Return CONSUMED if you don't want want the window insets to keep passing
+//            // down to descendant views.
+//            WindowInsetsCompat.CONSUMED
+//        }
     }
 
     override fun onResume() {
